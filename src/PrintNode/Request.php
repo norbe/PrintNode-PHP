946e109f2f447751a060842e2ba6e28908bc6296
Karel Hák <kareh@centrum.cz>
2017-07-31 11:05:39 +0200

    Added connection timeout

--- src/PrintNode/Request.php
+++ src/PrintNode/Request.php
@@ -315,6 +315,7 @@ class Request
         curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($curlHandle, CURLOPT_FOLLOWLOCATION, false);
         
         return $curlHandle;
